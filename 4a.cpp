1bb9447de6956a758d08369bc6045388063a89f3
Bright <brightsy04@gmail.com>
2026-02-08 03:34:14 -0800

    Color struct3

--- 4a.cpp
+++ 4a.cpp
@@ -1,3 +1,4 @@
+//COMSC210 | lab4A | Seoyeon An
 #include <iostream>
 #include <string>
 #include <vector>
@@ -20,11 +21,20 @@ void outputTable(const vector<Color>& colors);
 int main() {
     cout << "Vector of Structs: Color \n";
     srand(time(0));
-    cout << "Milestone 1- Test ONE initialized Color:\n";
+
+    cout << "Milestone 1- Test 1 initialized Color:\n";
     Color testColor;
     testColor.r = 192;
     testColor.g = 182;
     testColor.b = 96;
     outputColor(testColor);
+
+
+    //create empty vector
+    vector<Color> colors;
+    cout << "Milestone -2 Push 1 Color into vector:\n";
+    colors.push_back(testColor);
+    cout << "Vector size is now: " << colors.size() << "\n";
+    outputColor(colors[0]);
 
 }
