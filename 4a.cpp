9ec4adc165be6775df6dc8111e020ea331c7dfe8
Bright <brightsy04@gmail.com>
2026-02-08 03:31:15 -0800

    Color struct2

--- 4a.cpp
+++ 4a.cpp
@@ -18,5 +18,13 @@ void outputColor(const Color& c);
 void outputTable(const vector<Color>& colors);
 
 int main() {
-    cout << "=== Vector of Structs: Color ===\n\n";
+    cout << "Vector of Structs: Color \n";
+    srand(time(0));
+    cout << "Milestone 1- Test ONE initialized Color:\n";
+    Color testColor;
+    testColor.r = 192;
+    testColor.g = 182;
+    testColor.b = 96;
+    outputColor(testColor);
+
 }
